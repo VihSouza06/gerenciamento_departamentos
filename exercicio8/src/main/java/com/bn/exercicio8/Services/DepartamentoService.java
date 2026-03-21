1af0588bcfcb98a4914614d51adc2ac8bf195bc1
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-20 21:31:16 -0300

    Criação do Service

---
+++ exercicio8/src/main/java/com/bn/exercicio8/Services/DepartamentoService.java
@@ -0,0 +1,38 @@
+package com.bn.exercicio8.Services;
+
+import com.bn.exercicio8.Models.DepartamentoModel;
+import com.bn.exercicio8.Repositories.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartamentoService {
+
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    public List<DepartamentoModel> findAll(){
+        return departamentoRepository.findAll();
+    }
+
+    public DepartamentoModel criarDepartamento(DepartamentoModel departamentoModel){
+        return departamentoRepository.save(departamentoModel);
+    }
+
+    public void deletarDepartamento(Long id){
+        departamentoRepository.deleteById(id);
+    }
+
+    public DepartamentoModel buscarDepartamentoPorId(Long id){
+        return departamentoRepository.findById(id).get();
+    }
+
+    public DepartamentoModel atualizarDepartamento(Long id, DepartamentoModel departamentoModel){
+        DepartamentoModel newDepartamentoModel = departamentoRepository.findById(id).get();
+        return departamentoRepository.save(departamentoModel);
+
+    }
+
+}
