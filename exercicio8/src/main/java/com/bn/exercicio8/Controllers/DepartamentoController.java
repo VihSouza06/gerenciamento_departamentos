da17567fd3d55736b6bfbe5377e912453f5c597a
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-20 21:45:53 -0300

    Criação do Controller

---
+++ exercicio8/src/main/java/com/bn/exercicio8/Controllers/DepartamentoController.java
@@ -0,0 +1,53 @@
+package com.bn.exercicio8.Controllers;
+
+import com.bn.exercicio8.Models.DepartamentoModel;
+import com.bn.exercicio8.Services.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    @GetMapping
+    public ResponseEntity<List<DepartamentoModel>> findAll(){
+        List<DepartamentoModel> departamento = departamentoService.findAll();
+        return ResponseEntity.ok().body(departamento);
+    }
+
+    @GetMapping("/{id}")
+    public DepartamentoModel buscarDepartamentoPorId(@PathVariable Long id){
+        return departamentoService.buscarDepartamentoPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartamentoModel> criarDepartamento(@RequestBody DepartamentoModel departamentoModel){
+        DepartamentoModel departamentoCriado = departamentoService.criarDepartamento(departamentoModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(departamentoModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(departamentoCriado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarDepartamento(@PathVariable Long id){
+        departamentoService.deletarDepartamento(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DepartamentoModel> atualizarDepartamento
+            (@PathVariable Long id, @RequestBody DepartamentoModel departamentoModel){
+        DepartamentoModel departamentoAtualizado = departamentoService.atualizarDepartamento(id,departamentoModel);
+        return ResponseEntity.ok(departamentoAtualizado);
+    }
+
+
+}
